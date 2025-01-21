7f9172868862852f8112f42d44d7677003ee936e
issa-ezekiel <135858050+issa-ezekiel@users.noreply.github.com>
2025-01-21 14:02:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.header('Please upload a chest X-ray image')
 files = st.file_uploader('Upload Image', type=['jpeg', 'jpg', 'png'])
 
 # Load classifier model
-model = load_model(r"model/pneumonia_classifier.h5")  # Use raw string
+model = load_model(r"model\pneumonia_classifier.h5")  # Use raw string
 
 # Load class names 
 with open('./model/labels.txt', 'r') as f:
